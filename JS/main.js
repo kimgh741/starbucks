9b714f2a6af86d9c532f7d92e113566f3ddab499
kimgh741 <kimgh741@gmail.com>
2022-02-25 20:09:51 +0900

    '공통_모듈_분리'

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  // Logic.. 
-  searchInputEl.focus();
-});
-// focus가 될때 동작 하는 코드 
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-// focus 해제( blur )가 되면 동작 하는 코드 
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
 
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector('#to-top');
@@ -160,8 +142,3 @@ spyEls.forEach(function (spyEl) {
       .setClassToggle(spyEl,'show')
       .addTo(new ScrollMagic.Controller());
 });
-
-// 날짜 자동 계산 
-const thisYear = document.querySelector('.thisyear');
-// .textcontent 속성 : 해당 요소가 가지고 있는 글자 내용을 읽어 오거나 쓰기를 할 수 있음.  
-thisYear.textContent = new Date().getFullYear(); //  해당하는 년도를 생성함. 
